505c7398ddfe48c8f6ff32b655cce96affadccea
Com6235 <ru.nms199@gmail.com>
2023-12-03 12:01:15 +0300

    remade /sort

--- src/main/java/io/github/com6235/examplechek/Main.java
+++ src/main/java/io/github/com6235/examplechek/Main.java
@@ -1,24 +1,20 @@
 package io.github.com6235.examplechek;
 
-import com.destroystokyo.paper.event.server.ServerTickStartEvent;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
-import org.bukkit.Server;
-import org.bukkit.command.Command;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.world.GenericGameEvent;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
 
 public final class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-        Bukkit.getCommandMap().register("", new SortCommand("sort"));
-
+        Bukkit.getCommandMap().register("", new SortCommand("sort", "Sorts inventory", "usageMessage", new ArrayList<String>()));
     }
 
     @EventHandler
--- src/main/java/io/github/com6235/examplechek/SortCommand.java
+++ src/main/java/io/github/com6235/examplechek/SortCommand.java
@@ -5,6 +5,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,269 +13,96 @@ import java.util.List;
 import java.util.Objects;
 
 public class SortCommand extends Command {
-    protected SortCommand(@NotNull String name) {
-        super(name);
+    protected SortCommand(@NotNull String name, @NotNull String description, @NotNull String usageMessage, @NotNull List<String> aliases) {
+        super(name, description, usageMessage, aliases);
     }
 
-    public ItemStack[] mainStorage = new ItemStack[] {};
-    protected boolean SortInstruments(ItemStack e, List<ItemStack> real, List<ItemStack> fake) {
-        boolean isSorted = false;
-        switch (e.getType()) {
-            case NETHERITE_AXE, DIAMOND_AXE, GOLDEN_AXE, IRON_AXE, STONE_AXE, WOODEN_AXE:
-                try {
-                    if (real.get(3) == null) {
-                        real.set(3, e);
-                    } else {
-                        fake.add(e);
-                    }
-                    isSorted = true;
-                    break;
-                } catch (IndexOutOfBoundsException ignored) {
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.set(3, e);
-                    isSorted = true;
-                    break;
-                }
+    public List<ItemStack> hotbar = new ArrayList<>();
+    public List<ItemStack> inventory = new ArrayList<>();
+    public List<ItemStack> sortedHotbar = new ArrayList<>();
+    public List<ItemStack> sortedInventory = new ArrayList<>();
+    public List<ItemStack> sorted = new ArrayList<>();
 
-            case NETHERITE_PICKAXE, DIAMOND_PICKAXE, GOLDEN_PICKAXE, IRON_PICKAXE, STONE_PICKAXE, WOODEN_PICKAXE:
-                try {
-                    if (real.get(2) == null) {
-                        real.set(2, e);
-                    } else {
-                        fake.add(e);
-                    }
-                    isSorted = true;
-                    break;
-                } catch (IndexOutOfBoundsException ignored) {
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.set(2, e);
-                    isSorted = true;
-                    break;
-                }
-
-            case NETHERITE_SWORD, DIAMOND_SWORD, GOLDEN_SWORD, IRON_SWORD, STONE_SWORD, WOODEN_SWORD:
-                try {
-                    if (real.get(1) == null) {
-                        real.set(1, e);
-                    } else {
-                        fake.add(e);
-                    }
-                    isSorted = true;
-                    break;
-                } catch (IndexOutOfBoundsException ignored) {
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.set(1, e);
-                    isSorted = true;
-                    break;
-                }
-
-            case NETHERITE_SHOVEL, DIAMOND_SHOVEL, GOLDEN_SHOVEL, IRON_SHOVEL, STONE_SHOVEL, WOODEN_SHOVEL:
-                try {
-                    if (real.get(4) == null) {
-                        real.set(4, e);
-                    } else {
-                        fake.add(e);
-                    }
-                    isSorted = true;
-                    break;
-                } catch (IndexOutOfBoundsException ignored) {
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.set(4, e);
-                    isSorted = true;
-                    break;
-                }
-
-            case NETHERITE_HOE, DIAMOND_HOE, GOLDEN_HOE, IRON_HOE, STONE_HOE, WOODEN_HOE:
-                try {
-                    if (real.get(5) == null) {
-                        real.set(5, e);
-                    } else {
-                        fake.add(e);
-                    }
-                    isSorted = true;
-                    break;
-                } catch (IndexOutOfBoundsException ignored) {
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.set(5, e);
-                    isSorted = true;
-                    break;
-                }
+    public void sort(List<ItemStack> srt, List<ItemStack> toSort, CommandSender sender) {
+        ItemStack prev = null;
+        List<ItemStack> unsorted = new ArrayList<>();
+        for (int idx = 0; idx < toSort.size(); idx++) {
+            ItemStack i = toSort.get(idx);
+            if (prev == null) {
+                prev = i;
+                srt.add(i);
+                sender.sendMessage("Prev - null");
+            } else if (prev.getType() == i.getType()) {
+                srt.add(i);
+                sender.sendMessage("Sorted: %s".formatted(i));
+            } else if (prev.getType() != i.getType()) {
+                unsorted.add(i);
+                sender.sendMessage("UnSorted: %s".formatted(i));
+            }
         }
-        return isSorted;
-    }
-
-    protected boolean SortArmor(ItemStack e, List<ItemStack> real, List<ItemStack> fake) {
-        boolean isSorted = false;
-        switch (e.getType()) {
-            case NETHERITE_HELMET, DIAMOND_HELMET, GOLDEN_HELMET, IRON_HELMET, CHAINMAIL_HELMET, LEATHER_HELMET, TURTLE_HELMET:
-                try {
-                    if (real.get(4) == null) {
-                        real.set(4, e);
-                    } else {
-                        fake.add(e);
-                    }
-                    isSorted = true;
-                    break;
-                } catch (IndexOutOfBoundsException ignored) {
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.set(4, e);
-                    isSorted = true;
-                    break;
-                }
-
-            case NETHERITE_CHESTPLATE, DIAMOND_CHESTPLATE, GOLDEN_CHESTPLATE, IRON_CHESTPLATE, CHAINMAIL_CHESTPLATE, LEATHER_CHESTPLATE:
-                try {
-                    if (real.get(3) == null) {
-                        real.set(3, e);
-                    } else {
-                        fake.add(e);
-                    }
-                    isSorted = true;
-                    break;
-                } catch (IndexOutOfBoundsException ignored) {
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.set(3, e);
-                    isSorted = true;
-                    break;
-                }
-
-            case NETHERITE_LEGGINGS, DIAMOND_LEGGINGS, GOLDEN_LEGGINGS, IRON_LEGGINGS, CHAINMAIL_LEGGINGS, LEATHER_LEGGINGS:
-                try {
-                    if (real.get(2) == null) {
-                        real.set(2, e);
-                    } else {
-                        fake.add(e);
-                    }
-                    isSorted = true;
-                    break;
-                } catch (IndexOutOfBoundsException ignored) {
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.set(2, e);
-                    isSorted = true;
-                    break;
-                }
-
-            case NETHERITE_BOOTS, DIAMOND_BOOTS, GOLDEN_BOOTS, IRON_BOOTS, CHAINMAIL_BOOTS, LEATHER_BOOTS:
-                try {
-                    if (real.get(1) == null) {
-                        real.set(1, e);
-                    } else {
-                        fake.add(e);
-                    }
-                    isSorted = true;
-                    break;
-                } catch (IndexOutOfBoundsException ignored) {
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.set(1, e);
-                    isSorted = true;
-                    break;
-                }
-
-            case SHIELD:
-                try {
-                    if (real.get(5) == null) {
-                        real.set(5, e);
-                    } else {
-                        fake.add(e);
-                    }
-                    isSorted = true;
-                    break;
-                } catch (IndexOutOfBoundsException ignored) {
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.add(null);
-                    real.set(5, e);
-                    isSorted = true;
-                    break;
-                }
+        if (!unsorted.isEmpty()) {
+            sort(srt, unsorted, sender);
         }
-
-        return isSorted;
     }
 
     @Override
     public boolean execute(@NotNull CommandSender sender, @NotNull String commandLabel, @NotNull String[] args) {
-        mainStorage = Objects.requireNonNull(Bukkit.getPlayer(sender.getName())).getInventory().getContents();
-        List<ItemStack> blockStack = new ArrayList<>() {};
-        List<ItemStack> itemStack = new ArrayList<>() {};
-        List<ItemStack> instrumentStack = new ArrayList<>() {};
-        List<ItemStack> secondInstrumentStack = new ArrayList<>() {};
-        List<ItemStack> armorShieldStack = new ArrayList<>() {};
-        List<ItemStack> secondArmorShieldStack = new ArrayList<>() {};
-
-        List<ItemStack> realStack = new ArrayList<>() {};
-
-        for (ItemStack element : mainStorage) {
+        // Сброс переменных
+        sortedInventory = new ArrayList<>();
+        sortedHotbar = new ArrayList<>();
+        sorted = new ArrayList<>();
+        inventory = new ArrayList<>();
+        hotbar = new ArrayList<>();
+        // Получение инвентаря
+        ItemStack[] mainStorage = Objects.requireNonNull(Bukkit.getPlayer(sender.getName())).getInventory().getStorageContents();
+        // инит
+        List<ItemStack> hb_blockStack = new ArrayList();
+        List<ItemStack> hb_itemStack = new ArrayList<>();
+        List<ItemStack> iv_blockStack = new ArrayList();
+        List<ItemStack> iv_itemStack = new ArrayList<>();
+        // Расфасовка
+        hotbar = List.of(Arrays.copyOfRange(mainStorage, 0, 9));
+        sender.sendMessage(Arrays.toString(mainStorage));
+        sender.sendMessage(String.valueOf(mainStorage.length));
+        for (int indexer = 9; indexer < mainStorage.length; indexer++) {
+            if (mainStorage[indexer] != null) {
+                inventory.add(mainStorage[indexer]);
+                sender.sendMessage(String.valueOf(inventory.size()));
+            }
+        }
+        // сортировка
+        for (ItemStack element : hotbar) {
             if (element != null) {
                 if (element.getType().isBlock()) {
-                    blockStack.add(element);
+                    hb_blockStack.add(element);
                 } else if (element.getType().isItem()) {
-                    boolean s = SortInstruments(element, instrumentStack, secondInstrumentStack);
-                    boolean a = SortArmor(element, armorShieldStack, secondArmorShieldStack);
-                    if (!s && !a) {
-                        itemStack.add(element);
-                    }
+                    hb_itemStack.add(element);
                 }
             }
         }
-
-
-        realStack.addAll(instrumentStack);
-        realStack.addAll(blockStack);
-        realStack.addAll(itemStack);
-        realStack.addAll(secondInstrumentStack);
-        realStack.addAll(secondArmorShieldStack);
-        realStack.removeIf(Objects::isNull);
-        realStack.addAll(realStack.size(),armorShieldStack);
-        for (int i = 0; i < 42 - realStack.size(); i++) {
-            realStack.add(realStack.size() - 5, null);
+        sort(sortedHotbar, hb_blockStack, sender);
+        sort(sortedHotbar, hb_itemStack, sender);
+        for (ItemStack element : inventory) {
+            if (element != null) {
+                if (element.getType().isBlock()) {
+                    iv_blockStack.add(element);
+                } else if (element.getType().isItem()) {
+                    iv_itemStack.add(element);
+                }
+            }
         }
-        /*sender.sendMessage(Arrays.toString(realStack.toArray(new ItemStack[41])));*/
-        Objects.requireNonNull(Bukkit.getPlayer(sender.getName())).getInventory().setContents(realStack.toArray(new ItemStack[41]));
+        sort(sortedInventory, iv_blockStack, sender);
+        sort(sortedInventory, iv_itemStack, sender);
+
+        sender.sendMessage(Arrays.toString(sortedHotbar.toArray(new ItemStack[9])));
+        sender.sendMessage(Arrays.toString(sortedInventory.toArray(new ItemStack[26])));
+
+        sender.sendMessage(String.valueOf(sortedHotbar.size()));
+        sender.sendMessage(String.valueOf(sortedInventory.size()));
+        // Соединение отсортированных хотбара и инвентаря
+        sorted.addAll(sortedHotbar);
+        sorted.addAll(sortedInventory);
+        Objects.requireNonNull(Bukkit.getPlayer(sender.getName())).getInventory().setContents(sorted.toArray(new ItemStack[41]));
         return false;
     }
 }
